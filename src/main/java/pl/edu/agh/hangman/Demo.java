c70ceac788dd313db8f1cefe7b97377c810147e6
Anna Madej <annamadejkrakow@gmail.com>
2020-03-01 10:44:42 +0100

    boardprinter

--- src/main/java/pl/edu/agh/hangman/Demo.java
+++ src/main/java/pl/edu/agh/hangman/Demo.java
@@ -2,8 +2,10 @@ package pl.edu.agh.hangman;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class Demo {
 
@@ -19,6 +21,18 @@ public class Demo {
         TriesCounter triesCounter = new TriesCounter();
         WordChecker wordChecker = new WordChecker(triesCounter);
         hangman = new Hangman(wordsPicker, boardPrinter, userInputReceiver, triesCounter, wordChecker);
+        
+        Set<Integer> guessed = new HashSet<>();
+        
+        
+        guessed.add(1);
+        try {
+			boardPrinter.printHangMan("Adres e-mail", guessed, 2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
  
     }
 }
