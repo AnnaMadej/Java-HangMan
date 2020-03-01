f60601e6903759975ed4cfb7f4dc4059f0f46dd3
Anna Madej <annamadejkrakow@gmail.com>
2020-03-01 11:14:51 +0100

    hangman

--- src/main/java/pl/edu/agh/hangman/Demo.java
+++ src/main/java/pl/edu/agh/hangman/Demo.java
@@ -2,22 +2,38 @@ package pl.edu.agh.hangman;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
 
 public class Demo {
 
-<<<<<<< HEAD
-  
-=======
     private static WordsLoader wordsLoader;
     private static Hangman hangman;
 
     public static void main(String[] arg) {
-    	
-    	
+
+        wordsLoader = new WordsLoader();
+        WordsPicker wordsPicker = new WordsPicker();
+        BoardPrinter boardPrinter = new BoardPrinter();
+        UserInputReceiver userInputReceiver = new UserInputReceiver();
+        TriesCounter triesCounter = new TriesCounter();
+        WordChecker wordChecker = new WordChecker(triesCounter);
+        hangman = new Hangman(wordsPicker, boardPrinter, userInputReceiver, triesCounter, wordChecker);
+
+        try {
+            List<String> words = wordsLoader.loadWords("src/main/resources/slowa.txt");
+            wordsPicker.setWords(words);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie można załadować listy słów z pliku!");
+        }
+
+        try {
+            hangman.play();
+        } catch (NoSuchElementException e) {
+            System.out.println("Lista słów nie istnieje!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
->>>>>>> 8c4d40090a68750e8cbe13925bdb6d3dd83f9bfb
 }
--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,60 +1,44 @@
 package pl.edu.agh.hangman;
 
+import java.io.IOException;
+
 public class Hangman {
 
-    public static final String[] HANGMANPICS = new String[]{
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    "  |   |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    " /|   |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    " /|\\  |\n" +
-                    "      |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    " /|\\  |\n" +
-                    " /    |\n" +
-                    "      |\n" +
-                    "=========",
-            "  +---+\n" +
-                    "  |   |\n" +
-                    "  O   |\n" +
-                    " /|\\  |\n" +
-                    " / \\  |\n" +
-                    "      |\n" +
-                    "========"
-    };
+    private WordsPicker wordsPicker;
+    private BoardPrinter boardPrinter;
+    private UserInputReceiver userInputReceiver;
+    private TriesCounter triesCounter;
+    private WordChecker wordChecker;
 
-    public static void main(String[] args) {
-    	
+    public void play() throws IOException {
+        while (true) {
+            playNewWord();
+        }
+    }
+
+    private void playNewWord() throws IOException {
+        String currentWord = wordsPicker.pickAWord();
+        wordChecker.startNewWord(currentWord);
+
+        do {
+            boardPrinter.printHangMan(wordChecker.getCurrentWord(), wordChecker.getGuessedLetters(), triesCounter.getTryNumber() );
+            char userInput = userInputReceiver.inputFirstLetter();
+            wordChecker.guessLetter(userInput);
+
+        } while (wordChecker.getGuessedLetters().size() != wordChecker.getCurrentWord().length() && triesCounter.getTryNumber() < triesCounter.getMaxNumberOfTries());
+        boardPrinter.printHangMan(wordChecker.getCurrentWord(), wordChecker.getGuessedLetters(), triesCounter.getTryNumber() );
+
+
+        boolean isWinner = triesCounter.checkIfWinner();
+        boardPrinter.printResult(isWinner);
+    }
+
+
+    public Hangman(WordsPicker wordsPicker, BoardPrinter boardPrinter, UserInputReceiver userInputReceiver, TriesCounter triesCounter, WordChecker wordChecker) {
+        this.wordsPicker = wordsPicker;
+        this.boardPrinter = boardPrinter;
+        this.userInputReceiver = userInputReceiver;
+        this.triesCounter = triesCounter;
+        this.wordChecker = wordChecker;
     }
 }
